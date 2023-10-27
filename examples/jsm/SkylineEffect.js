9187d479b1d708790d17669ca120b220b145ebdb
wzw <wzw9803@163.com>
2023-10-27 17:35:09 +0800

    feat: add skyline effect

---
+++ examples/jsm/SkylineEffect.js
@@ -0,0 +1,145 @@
+import { ShaderPostPass, ATTACHMENT, Color3 } from 't3d';
+import { Effect, defaultVertexShader } from 't3d-effect-composer';
+
+export default class SkylineEffect extends Effect {
+
+	constructor() {
+		super();
+
+		this.bufferDependencies = [
+			{ key: 'GBuffer' }
+		];
+
+		this.lineColor = new Color3().setHex(0xff0000);
+		this.lineWidth = 1.0;
+		this.threshold = 0.1;
+
+		this._skylinePass = new ShaderPostPass(SkylineShader);
+		this._skylinePass.material.depthTest = false;
+		this._skylinePass.material.depthWrite = false;
+	}
+
+	resize(width, height) {
+		this._skylinePass.uniforms.resolution[0] = 1 / width;
+		this._skylinePass.uniforms.resolution[1] = 1 / height;
+	}
+
+	render(renderer, composer, inputRenderTarget, outputRenderTarget, finish) {
+		const gBuffer = composer.getBuffer('GBuffer');
+		const gBufferRenderStates = gBuffer.getCurrentRenderStates();
+
+		const projectionMatrix = gBufferRenderStates.camera.projectionMatrix;
+		const cameraNear = gBufferRenderStates.camera.near;
+		const cameraFar = gBufferRenderStates.camera.far;
+
+		const skylinePass = this._skylinePass;
+
+		skylinePass.material.uniforms.colorTex = inputRenderTarget.texture;
+		skylinePass.material.uniforms.depthTex = gBuffer.output()._attachments[ATTACHMENT.DEPTH_STENCIL_ATTACHMENT];
+		skylinePass.material.uniforms.cameraNear = cameraNear;
+		skylinePass.material.uniforms.cameraFar = cameraFar;
+		projectionMatrix.toArray(skylinePass.material.uniforms.projection);
+
+		skylinePass.material.uniforms.threshold = this.threshold;
+		skylinePass.material.uniforms.lineWidth = this.lineWidth;
+		this.lineColor.toArray(skylinePass.material.uniforms.lineColor);
+
+		renderer.setRenderTarget(outputRenderTarget);
+		renderer.setClearColor(0, 0, 0, 0);
+		if (finish) {
+			renderer.clear(composer.clearColor, composer.clearDepth, composer.clearStencil);
+		} else {
+			renderer.clear(true, true, false);
+		}
+
+		if (finish) {
+			skylinePass.material.transparent = composer._tempClearColor[3] < 1 || !composer.clearColor;
+			skylinePass.renderStates.camera.rect.fromArray(composer._tempViewport);
+		}
+		skylinePass.render(renderer);
+		if (finish) {
+			skylinePass.material.transparent = false;
+			skylinePass.renderStates.camera.rect.set(0, 0, 1, 1);
+		}
+	}
+
+	dispose() {
+		this._skylinePass.dispose();
+	}
+
+}
+
+const SkylineShader = {
+	name: 'ec_skyline',
+	defines: {},
+	uniforms: {
+		colorTex: null,
+		depthTex: null,
+		cameraNear: 0.1,
+		cameraFar: 0.1,
+		projection: new Float32Array(16),
+		resolution: [1.0, 1.0],
+		lineColor: [1.0, 0.0, 0.0],
+		lineWidth: 1.0,
+		threshold: 0.1
+	},
+	vertexShader: defaultVertexShader,
+	fragmentShader: `
+		uniform sampler2D depthTex;
+		uniform sampler2D colorTex;
+		
+		uniform mat4 projection;
+		uniform float cameraNear;
+		uniform float cameraFar;
+		uniform vec2 resolution;
+
+		uniform vec3 lineColor;
+		uniform float lineWidth;
+		uniform float threshold;
+		
+		varying vec2 v_Uv;
+
+		float fetchDepth(vec2 uv) {
+			vec4 depthTexel = texture2D(depthTex, uv);
+			return depthTexel.r * 2.0 - 1.0;
+		}
+
+		float viewZToOrthographicDepth(const in float viewZ, const in float near, const in float far) {
+			return (viewZ + near) / (near - far);
+		}
+
+		float perspectiveDepthToViewZ(const in float invClipZ, const in float near, const in float far) {
+			return (near * far) / ((far - near) * invClipZ - far);
+		}
+
+		float getLinearDepth(vec2 coord) {
+			float depth = fetchDepth(coord);
+			float viewZ = perspectiveDepthToViewZ(depth, cameraNear, cameraFar);
+          	return viewZToOrthographicDepth(viewZ, cameraNear, cameraFar);
+		}
+
+		void main() {
+			float depth = getLinearDepth(v_Uv);
+
+			vec2 offsets[8];
+			offsets[0] = vec2(-1, -1);
+			offsets[1] = vec2(0, -1);
+			offsets[2] = vec2(1, -1);
+			offsets[3] = vec2(-1, 0);
+			offsets[4] = vec2(1, 0);
+			offsets[5] = vec2(-1, 1);
+			offsets[6] = vec2(0, 1);
+			offsets[7] = vec2(1, 1);
+
+			float total = 0.0;
+			for (int i = 0; i < 8; i++) {
+				float sampleDepth = getLinearDepth(v_Uv + (offsets[i] * resolution * lineWidth));
+				total += abs(sampleDepth - depth);
+			}
+			total /= 8.0;
+
+			float factor = smoothstep(threshold, threshold * 2.0, total);
+			gl_FragColor = vec4(mix(texture2D( colorTex, v_Uv ).rgb, lineColor, factor), 1.0);
+		}
+	`
+};
